3c7b437dc93fd77f608a8cdfdedf84b24d5c4d16
sylviamuturi7 <sylviamuturi7@gmail.com>
2025-11-27 21:40:57 +0300

    statementslab

--- gardenManagement.js
+++ gardenManagement.js
@@ -1,3 +1,23 @@
 const temperature = 90;
 const timeOfDay = "morning";
 let soilMoisture = 30;
+
+//Watering Control 
+if (temperature > 80) {
+    console.log("Watering on");
+} else {
+    console.log("Watering off");
+}
+
+//Garden Lights Control
+if (timeOfDay === "evening" || timeOfDay === "night") {
+    console.log("Lights on");
+} else {
+    console.log("Lights off");
+}
+
+//Soil Moisture Adjustment
+while (soilMoisture < 40) {
+    soilMoisture += 5;
+    console.log(40);
+}
